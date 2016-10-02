95cc714d32a75760a03fab87fc0d0f856fea2abc
Michael Glenn <dmichaelglenn@gmail.com>
2016-10-01 20:06:54 -0700

    added get functionalities

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
+require('dotenv').load({ silent: true });
+
 var express = require("express");
 var path = require("path");
 var bodyParser = require("body-parser");
-var mongodb = require("mongodb");
-var mongoClient = mongo.mongoClient;
-var ObjectID = mongodb.ObjectID;
+var mongoose = require('mongoose');
+// var mongodb = require("mongodb");
+// var mongoClient = mongo.mongoClient;
+// var ObjectID = mongodb.ObjectID;
+
+
+mongoose.connect(process.env.MONGODB_URI);
+
+var AlbumSchema = mongoose.Schema({
+    name: String
+});
+
+var Album = mongoose.model('Album', AlbumSchema);
+
 
 var ALBUMS_COLLECTION = "albums";
 
@@ -12,45 +25,38 @@ app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.json());
 
 // Create a database variable outside of the database connection callback to reuse the connection pool in your app.
-var db;
-
-// Connect to the database before starting the application server.
-mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
-  if (err) {
-    console.log(err);
-    process.exit(1);
-  }
-
-  // Save database object from the callback for reuse.
-  db = database;
-  console.log("Database connection ready");
-
-  // Initialize the app.
-  var server = app.listen(process.env.PORT || 8080, function () {
-    var port = server.address().port;
-    console.log("App now running on port", port);
-  });
-});
-
-// Generic error handler used by all endpoints.
-function handleError(res, reason, message, code) {
-  console.log("ERROR: " + reason);
-  res.status(code || 500).json({"error": message});
-}
 
 /*  "/contacts"
  *    GET: finds all contacts
  *    POST: creates a new contact
  */
 
-app.get("/albums", function(req, res) {
-    var albums = db.albums;
-    albums.find({}).toArray(function(err, results) {
-        if(err) {
-            res.status(500).send(err);
-        }
-    })
-});
+function listResources (Model) {
+    return function (req, res, next) {
+        Model.find(function (err, docs) {
+            if (err) return next(err);
+            res.json(docs);
+        });
+    };
+}
+
+function getResource (Model, paramId) {
+    return function (req, res, next) {
+        Model.findOne({_id:req.params[paramId]}, function (err, doc) {
+            if (err) return next(err);
+            res.json(doc);
+        });
+    };
+}
+
+
+
+
+
+
+
+
+app.get("/albums", listResources(Album));
 
 app.post("/albums", function(req, res) {
 });
@@ -61,11 +67,25 @@ app.post("/albums", function(req, res) {
  *    DELETE: deletes contact by id
  */
 
-app.get("/albums/:id", function(req, res) {
-});
+app.get("/albums/:album", getResource(Album, 'album'));
 
 app.put("/albums/:id", function(req, res) {
 });
 
 app.delete("/albums/:id", function(req, res) {
+});
+
+app.use(function (req, res, next) {
+    res.status(404).json({ error: 'Resource Not Found' });
+});
+
+app.use(function (err, req, res, next) {
+    console.error(err);
+    res.status(500).json({ error: err });
+});
+
+// Initialize the app.
+var server = app.listen(process.env.PORT || 8080, function () {
+  var port = server.address().port;
+  console.log("App now running on port", port);
 });
